a547a9fa3dd06d4ec6eed7c51b4d9bcc29b0b60e
JulianArnold <julian_arnold@hotmail.com>
2020-12-14 22:56:21 +0000

    working on variable re-use and how it impacts scope

--- ex35.rb
+++ ex35.rb
@@ -1,5 +1,5 @@
 def gold_room
-  puts "There is a room full of gold, how much do you take?"
+  puts "This room is full of gold.  How much will you take?"
 
   print "> "
   choice = $stdin.gets.chomp
@@ -11,7 +11,7 @@ def gold_room
   end
 
   if how_much < 50
-    puts "Nice, you're not greedy.  You win!"
+    puts "That's not greedy, you win!"
     exit(0)
   else
     dead("Now, that's just greedy!")
@@ -19,9 +19,9 @@ def gold_room
 end
 
 def bear_room
-  puts "There is a bear here!"
+  puts "There's a bear in here!"
   puts "He has a pot of honey.."
-  puts "The plump bear is in front of another door.."
+  puts "The large bear sits in front of another door.."
   puts "How will you move the bear?"
   bear_moved = false
 
@@ -30,23 +30,23 @@ def bear_room
     choice = $stdin.gets.chomp
 
     if choice == "take honey"
-      dead("The bear looks at you then slaps your face off!")
+      dead("The bear looks at you a rips your face off!")
     elsif choice == "taunt bear" && !bear_moved
-      puts "The bear has moved and you can go through the door..."
+      puts "The bear has moved from the door, you can through it now.."
       bear_moved = true
     elsif choice == "taunt bear" && bear_moved
-      dead("The bear gets pissed off and chews off your leg!")
+      dead("The bear gets pissed off and chews off your leg")
     elsif choice == "open door" && bear_moved
       gold_room
     else
-      puts "I've no idea what that means, please try again"
+      puts "I don't know what that means, please try again"
     end
   end
 end
 
 def cthulhu_room
-  puts "Here you see the great evil Cthulhu.."
-  puts "He, She, whatever stares at you and you go insane!"
+  puts "Here you see the great, evil Cthulhu"
+  puts "He, it whatever stares at you and you go insane"
   puts "Do you flee for your life or eat your head?"
 
   print "> "
@@ -55,7 +55,7 @@ def cthulhu_room
   if choice.include? "flee"
     start
   elsif choice.include? "head"
-    dead("Well, that was tasty!")
+    dead("Well, that was tasty")
   else
     cthulhu_room
   end
@@ -67,8 +67,8 @@ def dead(why)
 end
 
 def start
-  puts "You are in a dark room.."
-  puts "There is a door to your left and to your right.."
+  puts "You are in a dark room"
+  puts "There are doors to your left and to your right"
   puts "Which one do you take?"
 
   print "> "
@@ -79,7 +79,7 @@ def start
   elsif choice == "right"
     cthulhu_room
   else
-    dead("You stumble around the room until you starve..")
+    dead("You stumble around until you starve")
   end
 end
 
