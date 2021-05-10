b30d8f5e06aea8d0bb611cb7fd8b6fbb20e29442
JulianArnold <julian_arnold@hotmail.com>
2021-05-10 22:58:58 +0100

    Vienna is done, was a short one and started with Tangier

--- ex36/tangier.rb
+++ ex36/tangier.rb
@@ -1,3 +1,5 @@
 module Tangier
-
+  def Tangier.pushkins_gambit
+    puts "007 is an exceptionally fine shot!"
+  end
 end
--- ex36/vienna.rb
+++ ex36/vienna.rb
@@ -6,6 +6,22 @@ module Vienna
     puts "Having left the cafe, Bond returns following an accident which was Saunders' demise."
     puts "Necros is responsible and left a 'Smiert Spionam' message on one of his balloons (disguise)"
     puts "Furious, Bond finds Kara tells her to get packed as he acknowledges he 'got the message'"
-    puts ""
+    puts "To get to Tangier, how many balloons did Necros have?"
+
+    print "007> "
+    number = $stdin.gets.chomp
+
+    if number =~ /\d+/
+      balloons = number.to_i
+    else
+      dead("Bond, learn to type a number!")
+    end
+
+    if balloons < 20
+      puts "Well done, off to Tangier to face Koskov && Whitaker."
+      Tangier.pushkins_gambit
+    else
+      dead("Necros gets you too!")
+    end
   end
 end
