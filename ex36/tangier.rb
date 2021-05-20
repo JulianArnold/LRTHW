ae367a5f28e980e481322d3442a42a857ac3b729
JulianArnold <julian_arnold@hotmail.com>
2021-05-20 23:21:23 +0100

    ex33 may have some hints for interesting ideas in ex36

--- ex36/tangier.rb
+++ ex36/tangier.rb
@@ -3,10 +3,17 @@ module Tangier
     puts "Bond and Kara arrive in Tangier and Bond sets out tracking Leonid Pushkin immediately."
     puts "snaring Pushkin in hotel room Bond confronts him on Smiert Spionam which LP denies"
     puts "revealing Koskov is evading arrest for embezzlement, LP and Bond hatch a cunning plan"
-    puts "Faking LP's assassination, Koskov & Whitaker proceed with their plan.."
-    puts "Kara cocks things up by contacting Koskov who tells her JB is a KGB spy. "
-    puts "Persuaded to drug Bond, he is captured by Koskov and Co."
+    puts "Faking LP's assassination, Koskov & Whitaker proceed with their own plan.."
+    puts "Kara cocks things up a little by contacting Koskov who tells her JB is a KGB spy. "
+    puts "Persuaded to drug Bond, he along with Kara is captured by Koskov and Co."
     puts ""
     puts ""
+    personnel = ['Bond', 'Kara', 'Koskov', 'Pushkin', 'Whitaker', 'Necros']
+
+
+
   end
 end
+
+
+# 20210520: looking at ex33 as possibility for the above
